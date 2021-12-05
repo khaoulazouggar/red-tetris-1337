1c927e44eb817bc6afc7f75d406fd44c41e8b0ea
Yassir Bolles <ysrbolles@gmail.com>
2021-12-06 00:02:21 +0100

    Fix tetris issues

--- client/src/Components/game.js
+++ client/src/Components/game.js
@@ -13,241 +13,246 @@ import PlayersStage from "./PlayersStage";
 import { socket } from "../socket/socket";
 
 function Game(props) {
-  const [username, setusername] = useState(props.data.username);
-  const [roomName, setroomName] = useState(props.data.roomName);
-  // const [start, setstart] = useState(true);
-  const start = props.data.start
-  const setstart = props.data.setstart
-  const [gameOver, setGameOver] = useState(false);
-  const [dropTime, setDropTime] = useState(null);
-  const [submited, setsubmited] = useState(true);
-  const gameRef = useRef(null);
-  const [tetriminos, setTetriminos] = useState([]);
-  const [player, nextPiece, updatePlayerPos, resetPlayer, playerRotate] =
-    usePlayer(setGameOver, setstart, setDropTime, tetriminos);
-  const [stage, nextStage, setStage, setNextStage, rowsCleared] = useStage(
-    player,
-    nextPiece,
-    resetPlayer,
-    gameOver
-  );
-  const [firstDrop, setfirstDrop] = useState(1);
-  const [score, setScore, rows, setRows, level, setLevel] =
-    useGameStatus(rowsCleared);
-  const [gameStarted, setGameStarted] = useState(false);
-  useEffect(() => {
-    socket.on("startGame", (tetriminos) => {
-      console.log("startGame", tetriminos);
-      setTetriminos(tetriminos);
-      tetriminos[0] && setGameStarted(true);
-    });
-  }, []);
+	const [username, setusername] = useState(props.data.username);
+	const [roomName, setroomName] = useState(props.data.roomName);
+	// const [start, setstart] = useState(true);
+	const start = props.data.start
+	const setstart = props.data.setstart
+	const [gameOver, setGameOver] = useState(false);
+	const [dropTime, setDropTime] = useState(null);
+	const [submited, setsubmited] = useState(true);
+	const gameRef = useRef(null);
+	const [tetriminos, setTetriminos] = useState([]);
 
-  useEffect(() => {
-    if (gameStarted) {
-      if (gameOver) {
-        console.log("bdina");
-        setStage(createStage());
-        setNextStage(createStage(4, 4));
-        resetPlayer();
-        setGameOver(false);
-      }
-      if (firstDrop === 1) {
-        resetPlayer();
-        setfirstDrop(2);
-      }
-      setstart(false);
-      setGameOver(false);
-      setDropTime(1000);
-      setScore(0);
-      setLevel(0);
-      setRows(0);
-    }
-  }, [gameStarted]);
-  useEffect(() => {
-    gameRef.current.focus();
-    props.data.clicked === 1
-      ? props.data.setclicked(2)
-      : props.data.setclicked(5);
-    // eslint-disable-next-line
-  }, []);
-
-  function handleChange(event) {
-    console.log(event.target.value);
-    props.data.setmode(event.target.value)
-  }
+	const [player, nextPiece, updatePlayerPos, resetPlayer, playerRotate] =
+		usePlayer(setGameOver, setstart, setDropTime, tetriminos, setTetriminos);
+	const [stage, nextStage, setStage, setNextStage, rowsCleared] = useStage(
+		player,
+		nextPiece,
+		resetPlayer,
+		gameOver
+	);
+	const [firstDrop, setfirstDrop] = useState(1);
+	const [score, setScore, rows, setRows, level, setLevel] =
+		useGameStatus(rowsCleared);
+	const [gameStarted, setGameStarted] = useState(false);
+	useEffect(() => {
+		socket.on("startGame", (tetris) => {
+			console.log("startGame", tetris);
+			setTetriminos(tetris);
+			tetris[0] && setGameStarted(true);
+		});
+	}, []);
+	useEffect(() => {
+		console.log("---------------------------", tetriminos);
+	}, [tetriminos]);
 
 
-  function startgame(e) {
-    if (e.key === "Enter" && submited) {
-      socket.emit("startgame", { room: props.data.roomName });
-    }
-  }
+	useEffect(() => {
+		if (gameStarted) {
+			if (gameOver) {
+				console.log("bdina");
+				setStage(createStage());
+				setNextStage(createStage(4, 4));
+				resetPlayer();
+				setGameOver(false);
+			}
+			if (firstDrop === 1) {
+				resetPlayer();
+				setfirstDrop(2);
+			}
+			setstart(false);
+			setGameOver(false);
+			setDropTime(1000);
+			setScore(0);
+			setLevel(0);
+			setRows(0);
+		}
+	}, [gameStarted]);
+	useEffect(() => {
+		gameRef.current.focus();
+		props.data.clicked === 1
+			? props.data.setclicked(2)
+			: props.data.setclicked(5);
+		// eslint-disable-next-line
+	}, []);
 
-  // This one starts the game
-  useInterval(() => {
-    drop();
-  }, dropTime);
+	function handleChange(event) {
+		console.log(event.target.value);
+		props.data.setmode(event.target.value)
+	}
 
-  const movePlayer = (dir) => {
-    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
-      updatePlayerPos({ x: dir, y: 0 });
-    }
-  };
 
-  const drop = () => {
-    // Increase level when player has cleared 10 rows
-    if (rows > (level + 1) * 3) {
-      setLevel((prev) => prev + 1);
-      // Also increase speed
-      setDropTime(1000 / (level + 1) + 200);
-    }
+	function startgame(e) {
+		if (e.key === "Enter" && submited) {
+			socket.emit("startgame", { room: props.data.roomName });
+		}
+	}
 
-    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
-      updatePlayerPos({ x: 0, y: 1, collided: false });
-    } else {
-      // Game over!
-      if (player.pos.y < 1) {
-        console.log("GAME OVER!!!");
-        setGameOver(true);
-        setstart(true);
-        setDropTime(null);
-      }
-      updatePlayerPos({ x: 0, y: 0, collided: true });
-    }
-  };
+	// This one starts the game
+	useInterval(() => {
+		drop();
+	}, dropTime);
 
-  const hardDrop = () => {
-    let tmp = 0;
-    while (!checkCollision(player, stage, { x: 0, y: tmp }))
-      tmp += 1
-    console.log(tmp)
-    updatePlayerPos({ x: 0, y: tmp - 1, collided: false });
+	const movePlayer = (dir) => {
+		if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+			updatePlayerPos({ x: dir, y: 0 });
+		}
+	};
 
-    // for (let i = 0; i < STAGE_HEIGHT; i++) {
-    //   if (checkCollision(player, stage, { x: 0, y: i })) {
-    //     tmp = i;
-    //     break;
-    //   }
-    // }
+	const drop = () => {
+		// Increase level when player has cleared 10 rows
+		if (rows > (level + 1) * 3) {
+			setLevel((prev) => prev + 1);
+			// Also increase speed
+			setDropTime(1000 / (level + 1) + 200);
+		}
 
-    // for (let i = tmp; i > 0; i--) {
-    //   if (!checkCollision(player, stage, { x: 0, y: i })) {
-    //     updatePlayerPos({ x: 0, y: i, collided: false });
-    //     break;
-    //   }
-    // }
-  };
+		if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+			updatePlayerPos({ x: 0, y: 1, collided: false });
+		} else {
+			// Game over!
+			if (player.pos.y < 1) {
+				console.log("GAME OVER!!!");
+				setGameOver(true);
+				setstart(true);
+				setDropTime(null);
+			}
+			updatePlayerPos({ x: 0, y: 0, collided: true });
+		}
+	};
 
-  const dropPlayer = () => {
-    // setDropTime(null);
-    drop();
-  };
+	const hardDrop = () => {
+		let tmp = 0;
+		while (!checkCollision(player, stage, { x: 0, y: tmp }))
+			tmp += 1
+		console.log(tmp)
+		updatePlayerPos({ x: 0, y: tmp - 1, collided: false });
 
-  const move = ({ keyCode }) => {
-    if (!gameOver) {
-      if (keyCode === 37) {
-        movePlayer(-1);
-      } else if (keyCode === 39) {
-        movePlayer(1);
-      } else if (keyCode === 40) {
-        dropPlayer();
-      } else if (keyCode === 38) {
-        playerRotate(stage, 1);
-      } else if (keyCode === 32) {
-        hardDrop();
-      }
-    }
-  };
+		// for (let i = 0; i < STAGE_HEIGHT; i++) {
+		//   if (checkCollision(player, stage, { x: 0, y: i })) {
+		//     tmp = i;
+		//     break;
+		//   }
+		// }
 
-  return (
-    <div
-      className="room-field"
-      onKeyPress={startgame}
-      tabIndex="0"
-      ref={gameRef}
-      onKeyDown={(e) => move(e)}
-    >
-      <div className="right-field" data-aos="fade-up" data-aos-duration="2000">
-        <div className="score next-field">
-          <p className="next-p">Next</p>
-          <div className="next">
-            {!start ? <NextPiece stage={nextStage} /> : ""}
-          </div>
-        </div>
-        <div className="chat left-chat">
-          <div className="players-field">
-            <div style={{ position: "relative", margin: "10px" }}>
-              <span>khaoula</span>
-              <PlayersStage stage={stage} />
-              <div className="players-overlay"></div>
-            </div>
-            <div style={{ position: "relative", margin: "10px" }}>
-              <span>yassir</span>
-              <PlayersStage stage={stage} />
-              <div className="players-overlay"></div>
-            </div>
-            <div style={{ position: "relative", margin: "10px" }}>
-              <span>yassir</span>
-              <PlayersStage stage={stage} />
-              <div className="players-overlay"></div>
-            </div>
-            <div style={{ position: "relative", margin: "10px" }}>
-              <span>khaoula</span>
-              <PlayersStage stage={stage} />
-              <div className="players-overlay"></div>
-            </div>
-          </div>
+		// for (let i = tmp; i > 0; i--) {
+		//   if (!checkCollision(player, stage, { x: 0, y: i })) {
+		//     updatePlayerPos({ x: 0, y: i, collided: false });
+		//     break;
+		//   }
+		// }
+	};
 
-        </div>
-      </div>
-      <div
-        className="left-field game"
-        data-aos="fade-down"
-        data-aos-duration="2000"
-      >
-        <Board data={{ start, setstart, stage, gameOver }} />
-      </div>
+	const dropPlayer = () => {
+		// setDropTime(null);
+		drop();
+	};
 
-      <div className="right-field" data-aos="fade-up" data-aos-duration="2000">
-        <div className="score">
-          <div className="score-level">
-            <div className="score-level-div">
-              <p className="score-p">Score</p>
-              <div className="score-num">{score}</div>
-            </div>
-            <div className="score-level-div">
-              <p className="score-p">level</p>
-              <div className="score-num">{level}</div>
-            </div>
-          </div>
-          <div className="mode-div">
-            <p className="score-p">mode</p>
-            <div className="">
-              <select className="mode-select" onChange={handleChange}>
-                <option value="solo" defaultValue>
-                  solo
-                </option>
-                <option value="batlle">batlle</option>
-              </select>
-            </div>
-          </div>
-        </div>
-        <div className="chat">
-          <Chat
-            data={{
-              username,
-              setusername,
-              roomName,
-              setroomName,
-              submited,
-              setsubmited,
-            }}
-          />
-        </div>
-      </div>
-    </div>
-  );
+	const move = ({ keyCode }) => {
+		if (!gameOver) {
+			if (keyCode === 37) {
+				movePlayer(-1);
+			} else if (keyCode === 39) {
+				movePlayer(1);
+			} else if (keyCode === 40) {
+				dropPlayer();
+			} else if (keyCode === 38) {
+				playerRotate(stage, 1);
+			} else if (keyCode === 32) {
+				hardDrop();
+			}
+		}
+	};
+
+	return (
+		<div
+			className="room-field"
+			onKeyPress={startgame}
+			tabIndex="0"
+			ref={gameRef}
+			onKeyDown={(e) => move(e)}
+		>
+			<div className="right-field" data-aos="fade-up" data-aos-duration="2000">
+				<div className="score next-field">
+					<p className="next-p">Next</p>
+					<div className="next">
+						{!start ? <NextPiece stage={nextStage} /> : ""}
+					</div>
+				</div>
+				<div className="chat left-chat">
+					<div className="players-field">
+						<div style={{ position: "relative", margin: "10px" }}>
+							<span>khaoula</span>
+							<PlayersStage stage={stage} />
+							<div className="players-overlay"></div>
+						</div>
+						<div style={{ position: "relative", margin: "10px" }}>
+							<span>yassir</span>
+							<PlayersStage stage={stage} />
+							<div className="players-overlay"></div>
+						</div>
+						<div style={{ position: "relative", margin: "10px" }}>
+							<span>yassir</span>
+							<PlayersStage stage={stage} />
+							<div className="players-overlay"></div>
+						</div>
+						<div style={{ position: "relative", margin: "10px" }}>
+							<span>khaoula</span>
+							<PlayersStage stage={stage} />
+							<div className="players-overlay"></div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+			<div
+				className="left-field game"
+				data-aos="fade-down"
+				data-aos-duration="2000"
+			>
+				<Board data={{ start, setstart, stage, gameOver }} />
+			</div>
+
+			<div className="right-field" data-aos="fade-up" data-aos-duration="2000">
+				<div className="score">
+					<div className="score-level">
+						<div className="score-level-div">
+							<p className="score-p">Score</p>
+							<div className="score-num">{score}</div>
+						</div>
+						<div className="score-level-div">
+							<p className="score-p">level</p>
+							<div className="score-num">{level}</div>
+						</div>
+					</div>
+					<div className="mode-div">
+						<p className="score-p">mode</p>
+						<div className="">
+							<select className="mode-select" onChange={handleChange}>
+								<option value="solo" defaultValue>
+									solo
+								</option>
+								<option value="batlle">batlle</option>
+							</select>
+						</div>
+					</div>
+				</div>
+				<div className="chat">
+					<Chat
+						data={{
+							username,
+							setusername,
+							roomName,
+							setroomName,
+							submited,
+							setsubmited,
+						}}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Game;
--- client/src/hooks/usePlayer.js
+++ client/src/hooks/usePlayer.js
@@ -4,107 +4,105 @@ import { TETROMINOS, randomTetromino } from "../Components/tetriminos";
 import { STAGE_WIDTH, checkCollision } from "../Components/gameHelpers";
 import { socket } from "../socket/socket";
 
-export const usePlayer = (setGameOver, setstart, setDropTime, tetriminos) => {
-  const [player, setPlayer] = useState({
-    pos: { x: 0, y: 0 },
-    tetromino: TETROMINOS[0].shape,
-    collided: false,
-  });
-  // const [tetriminos, setTetriminos] = useState([]);
-  const [nextTetromino, setNextTetromino] = useState("");
+export const usePlayer = (setGameOver, setstart, setDropTime, tetriminos, setTetriminos) => {
+	const [player, setPlayer] = useState({
+		pos: { x: 0, y: 0 },
+		tetromino: TETROMINOS[0].shape,
+		collided: false,
+	});
+	// const [tetriminos, setTetriminos] = useState([]);
+
+	useEffect(() => {
+		console.log("----------------------------------", tetriminos);
+		if (tetriminos[0] && tetriminos.length < 5) console.log("salam");
+	}, [tetriminos]);
+
+	const [nextPiece, setNextPiece] = useState({
+		pos: { x: 0, y: 0 },
+		tetromino: TETROMINOS[0].shape,
+		collided: false,
+	});
+
+	function rotate(matrix, dir) {
+		// Make the rows to become cols (transpose)
+		const mtrx = matrix.map((_, index) =>
+			matrix.map((column) => column[index])
+		);
+		// Reverse each row to get a rotaded matrix
+		if (dir > 0) return mtrx.map((row) => row.reverse());
+		return mtrx.reverse();
+	}
+
+	function playerRotate(stage, dir) {
+		const clonedPlayer = JSON.parse(JSON.stringify(player));
+		clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+		const pos = clonedPlayer.pos.x;
+		let offset = 1;
+		while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+			clonedPlayer.pos.x += offset;
+			offset = -(offset + (offset > 0 ? 1 : -1));
+			if (offset > clonedPlayer.tetromino[0].length) {
+				rotate(clonedPlayer.tetromino, -dir);
+				clonedPlayer.pos.x = pos;
+				return;
+			}
+		}
+		setPlayer(clonedPlayer);
+	}
+
+	const updatePlayerPos = ({ x, y, collided }) => {
+		setPlayer((prev) => ({
+			...prev,
+			pos: { x: (prev.pos.x += x), y: (prev.pos.y += y) },
+			collided,
+		}));
+	};
+
+	const shiftTetromino = () => {
+		tetriminos.shift();
+		console.log("++++++++++++++++++++", tetriminos);
+		setTetriminos(tetriminos);
+	}
 
 
+	const resetPlayer = useCallback((stage) => {
+		let tetris = {
+			pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+			tetromino: TETROMINOS[tetriminos[0]].shape,
+			collided: false,
+		};
 
-  // useEffect(() => {
-  //   socket.on("startGame", (tetriminos) => {
-  //     console.log("startGame", tetriminos);
-  //     setTetriminos(tetriminos);
-  //     setNextTetromino(tetriminos[1])
-  //     console.log("-----------------", TETROMINOS[tetriminos[0]].shape);
-  //   });
-  // }, []);
+		if (stage) {
+			if (!checkCollision(tetris, stage, { x: 0, y: 0 })) {
+				setPlayer({
+					pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+					tetromino: tetris.tetromino,
+					collided: false,
+				});
 
-  useEffect(() => {
-    console.log("----------------------------------", tetriminos);
-  }, [tetriminos]);
+			} else {
+				console.log("GAME OVER!!!");
+				setGameOver(true);
+				setstart(true);
+				setDropTime(null);
+			}
+		} else {
+			setPlayer({
+				pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+				tetromino: tetris.tetromino,
+				collided: false,
+			});
+		}
+		setNextPiece({
+			pos: { x: 0, y: 0 },
+			tetromino: TETROMINOS[tetriminos[1]].shape,
+			collided: false,
+		});
+		shiftTetromino()
+		// }
 
-  const [nextPiece, setNextPiece] = useState({
-    pos: { x: 0, y: 0 },
-    tetromino: TETROMINOS[0].shape,
-    collided: false,
-  });
+	}, [setGameOver, setstart, setDropTime, tetriminos, setTetriminos]);
 
-  function rotate(matrix, dir) {
-    // Make the rows to become cols (transpose)
-    const mtrx = matrix.map((_, index) =>
-      matrix.map((column) => column[index])
-    );
-    // Reverse each row to get a rotaded matrix
-    if (dir > 0) return mtrx.map((row) => row.reverse());
-    return mtrx.reverse();
-  }
-
-  function playerRotate(stage, dir) {
-    const clonedPlayer = JSON.parse(JSON.stringify(player));
-    clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
-
-    const pos = clonedPlayer.pos.x;
-    let offset = 1;
-    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
-      clonedPlayer.pos.x += offset;
-      offset = -(offset + (offset > 0 ? 1 : -1));
-      if (offset > clonedPlayer.tetromino[0].length) {
-        rotate(clonedPlayer.tetromino, -dir);
-        clonedPlayer.pos.x = pos;
-        return;
-      }
-    }
-    setPlayer(clonedPlayer);
-  }
-
-  const updatePlayerPos = ({ x, y, collided }) => {
-    setPlayer((prev) => ({
-      ...prev,
-      pos: { x: (prev.pos.x += x), y: (prev.pos.y += y) },
-      collided,
-    }));
-  };
-
-  const resetPlayer = useCallback((stage) => {
-    if (tetriminos[0]) {
-      let tetris = {
-        pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
-        tetromino: TETROMINOS[tetriminos[0]].shape,
-        collided: false,
-      };
-      if (stage) {
-        if (!checkCollision(tetris, stage, { x: 0, y: 0 })) {
-          setPlayer({
-            pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
-            tetromino: tetris.tetromino,
-            collided: false,
-          });
-        } else {
-          console.log("GAME OVER!!!");
-          setGameOver(true);
-          setstart(true);
-          setDropTime(null);
-        }
-      } else {
-        setPlayer({
-          pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
-          tetromino: tetris.tetromino,
-          collided: false,
-        });
-      }
-
-      setNextPiece({
-        pos: { x: 0, y: 0 },
-        tetromino: randomTetromino().shape,
-        collided: false,
-      });
-    }
-  }, [setGameOver, setstart, setDropTime]);
-
-  return [player, nextPiece, updatePlayerPos, resetPlayer, playerRotate];
+	return [player, nextPiece, updatePlayerPos, resetPlayer, playerRotate,];
 };
